e93c0174fb9bc5c78d5339afaec2b13e40d51574
Yokesh Jayakumar <karthj@uw.edu>
2019-08-03 12:03:54 -0700

    finished fully working app

--- src/index.js
+++ src/index.js
@@ -69,12 +69,20 @@ import './index.css';
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
+
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -85,22 +93,24 @@ import './index.css';
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
 
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
     
 
       const moves = history.map((step, move) => {
+          console.log(step + ", " + move);
           const desc = move?
             'Go to move #' + move :
             'Go to game start';
           return (
-              <li>
+              <li key={move}>
                   <button onClick={() => this.jumpTo(move)}>{desc}</button>
               </li>
           );
